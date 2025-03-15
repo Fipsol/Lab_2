6a1e15e52b0e6affe06df36368eff57ea00235a8
Filip <f.harapinski@gmail.com>
2025-03-15 19:14:56 +0100

    Poprawny program

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
         while (czyPrawda) {
             System.out.print("Wybierz program:\n1.Dodawanie\n2.Odejmowanie\n3.Mnożenie\n4.Dzielenie\n5.Zakończ program\n");
             int userChoice = scanner.nextInt();
-
             switch (userChoice) {
                 case 1:
                     System.out.print("Podaj liczbę A: ");
@@ -20,13 +19,36 @@ public class Main {
                     System.out.println("A + B = " + (a + b));
                     break;
                 case 2:
+                    System.out.print("Podaj liczbę A: ");
+                    a = scanner.nextFloat();
+                    System.out.print("Podaj liczbę B: ");
+                    b = scanner.nextFloat();
+                    System.out.println("A - B = " + (a - b));
+                    break;
                 case 3:
-                    case 4:
+                    System.out.print("Podaj liczbę A: ");
+                    a = scanner.nextFloat();
+                    System.out.print("Podaj liczbę B: ");
+                    b = scanner.nextFloat();
+                    System.out.println("A * B = " + (a * b));
+                    break;
+                case 4:
+                    System.out.print("Podaj liczbę A: ");
+                    a = scanner.nextFloat();
+                    System.out.print("Podaj liczbę B: ");
+                    b = scanner.nextFloat();
+                    if(b == 0){
+                        System.out.print("Nie można dzielić przez 0.\n");
+                        System.out.print("Podaj liczbę A: ");
+                        a = scanner.nextFloat();
+                        System.out.print("Podaj liczbę B: ");
+                        b = scanner.nextFloat();
+                    }
+                    System.out.println("A / B = " + (a / b));
+                    break;
                 case 5:
                     czyPrawda = false;
                     break;
-
-
             }
         }
     }
