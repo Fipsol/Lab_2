f7c1339b98738dbdcf26a50a8c2de3824c2123e2
Filip Harapiński <f.harapinski@gmail.com>
2025-03-15 21:47:14 +0100

    Zadanie "Suma A + B"

---
+++ src/Zad2_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Zad2_3 {
+    public static void main(String[] args) {
+    float a, b;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj liczbę A: ");
+    a = scanner.nextFloat();
+    System.out.println("Podaj liczbę B: ");
+    b = scanner.nextFloat();
+    System.out.println("A + B = " + (a+b));
+    }
+}
