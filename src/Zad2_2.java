131fba75f2e413dcfc96d4d5545473e4c747300e
Filip Harapiński <f.harapinski@gmail.com>
2025-03-15 21:46:49 +0100

    Zadanie "Liczby parzyste do 100"

---
+++ src/Zad2_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Zad2_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Jak się nazywasz?");
+        Scanner scanner = new Scanner(System.in);
+        String imie;
+        imie = scanner.next();
+        System.out.println("Cześć " + imie);
+    }
+}
