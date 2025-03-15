8d8de739bf19ff8d356aefa5992e966cf39a6026
Filip <f.harapinski@gmail.com>
2025-03-15 20:06:45 +0100

    Revert "Poprawny program"
    This reverts commit 37f81709ee70bd4cb473a4bbebe146134a83d039.

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,57 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    boolean czyPrawda = true;
+
+    public static float[] pobierzLiczby(){
+        float[] liczby = new float[2];
+        System.out.println("Podaj liczbę A: ");
+        liczby[0] = scanner.nextFloat();
+        System.out.println("Podaj liczbę B: ");
+        liczby[1] = scanner.nextFloat();
+        return liczby;
+    }
+    public static void dodajLiczba(float a, float b){
+    System.out.println("A + B = " + (a + b));
+    }
+    public static void odejmijLiczba(float a, float b){
+        System.out.println("A - B = " + (a - b));
+    }
+    public static void pomnozLiczba(float a, float b){
+        System.out.println("A * B = " + (a * b));
+    }
+    public static void podzielLiczba(float a, float b){
+        System.out.println("A / B = " + (a / b));
+    }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        float a, b;
+        float [] liczby;
         boolean czyPrawda = true;
-
-
         while (czyPrawda) {
             System.out.print("Wybierz program:\n1.Dodawanie\n2.Odejmowanie\n3.Mnożenie\n4.Dzielenie\n5.Zakończ program\n");
             int userChoice = scanner.nextInt();
+
             switch (userChoice) {
                 case 1:
-                    System.out.print("Podaj liczbę A: ");
-                    a = scanner.nextFloat();
-                    System.out.print("Podaj liczbę B: ");
-                    b = scanner.nextFloat();
-                    System.out.println("A + B = " + (a + b));
+                    liczby = pobierzLiczby();
+                    dodajLiczba(liczby[0], liczby[1]);
                     break;
                 case 2:
-                    System.out.print("Podaj liczbę A: ");
-                    a = scanner.nextFloat();
-                    System.out.print("Podaj liczbę B: ");
-                    b = scanner.nextFloat();
-                    System.out.println("A - B = " + (a - b));
+                    liczby = pobierzLiczby();
+                    odejmijLiczba(liczby[0], liczby[1]);
                     break;
                 case 3:
-                    System.out.print("Podaj liczbę A: ");
-                    a = scanner.nextFloat();
-                    System.out.print("Podaj liczbę B: ");
-                    b = scanner.nextFloat();
-                    System.out.println("A * B = " + (a * b));
+                    liczby = pobierzLiczby();
+                    pomnozLiczba(liczby[0], liczby[1]);
                     break;
                 case 4:
-                    System.out.print("Podaj liczbę A: ");
-                    a = scanner.nextFloat();
-                    System.out.print("Podaj liczbę B: ");
-                    b = scanner.nextFloat();
-                    if(b == 0){
+                    liczby = pobierzLiczby();
+                    while(liczby[1] == 0){
                         System.out.print("Nie można dzielić przez 0.\n");
-                        System.out.print("Podaj liczbę A: ");
-                        a = scanner.nextFloat();
-                        System.out.print("Podaj liczbę B: ");
-                        b = scanner.nextFloat();
+                        liczby =  pobierzLiczby();
                     }
-                    System.out.println("A / B = " + (a / b));
+                    podzielLiczba(liczby[0], liczby[1]);
                     break;
                 case 5:
                     czyPrawda = false;
